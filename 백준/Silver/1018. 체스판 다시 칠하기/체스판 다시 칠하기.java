eae15ca6d6bb96710df60ce44484ce79aba7c7a7
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-09-19 22:16:33 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 236 ms, Memory: 18836 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -11,6 +11,8 @@ public class Main {
         char[][][] compare = new char[(n-7)*(m-7)][8][8];
         int[] countW=new int[compare.length];
         int[] countB=new int[compare.length];
+        
+        //W부터 시작하는 8*8 배열(boardW) 생성
         for (int i = 0; i <8; i++) {
             for (int j = 0; j <8; j++) {
                 if(i%2==0&&j%2==0){
@@ -24,6 +26,8 @@ public class Main {
                 }
             }
         }
+        
+        //B부터 시작하는 8*8 배열(boardB) 생성
         for (int i = 0; i <8; i++) {
             for (int j = 0; j <8; j++) {
                 if(i%2==0&&j%2==0){
@@ -37,10 +41,14 @@ public class Main {
                 }
             }
         }
+        
+        //입력 받기
         for (int i = 0; i <n; i++) {
                 input[i]=sc.next();
         }
-        int a = 0;
+        
+        // compare 3차원 배열(시작점이 움직이는 모든 8*8 배열 경우의 수)를 생성
+        int a = 0; 
         for (int i = 0; i <= n-8 ; i++) {
             for (int j = 0; j <= m-8 ; j++) {
                 int b = 0;
@@ -55,7 +63,10 @@ public class Main {
                 a++;
             }
         }
-        for (int i = 0; i < compare.length; i++) {
+        
+        //compare배열과 boardW배열을 비교하여 서로 다른 문자의 갯수를 countW 배열에 입력함
+        //countW 배열은 각 시작점이 움직이는 모든 8*8배열 경우의수와의 차이를 배열화한 것 
+        for (int i = 0; i < compare.length; i++) { 
             for (int j = 0; j < compare[i].length; j++) {
                 for (int k = 0; k < compare[i][j].length; k++) {
                     if(compare[i][j][k]!=boardW[j][k]){
@@ -64,6 +75,9 @@ public class Main {
                 }
             }
         }
+        
+        //compare배열과 boardB배열을 비교하여 서로 다른 문자의 갯수를 countB 배열에 입력함
+        //countB 배열은 각 시작점이 움직이는 모든 8*8배열 경우의수와의 차이를 배열화한 것
         for (int i = 0; i < compare.length; i++) {
             for (int j = 0; j < compare[i].length; j++) {
                 for (int k = 0; k < compare[i][j].length; k++) {
@@ -73,8 +87,10 @@ public class Main {
                 }
             }
         }
-        Arrays.sort(countW);
-        Arrays.sort(countB);
-        System.out.println(Math.min(countW[0],countB[0]));
+        Arrays.sort(countW); //countW 배열 오름차순 정렬
+        Arrays.sort(countB); //countB 배열 오름차순 정렬
+        
+        //각 배열의 최솟값끼리 비교하여 총 최솟값 출력
+        System.out.println(Math.min(countW[0],countB[0])); 
     }
 }
