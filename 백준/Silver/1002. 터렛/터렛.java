b51fbbe8f248e19c9c0a20067cc440dd0a7156bf
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-09-22 00:09:01 +0900

    [Silver III] Title: 터렛, Time: 260 ms, Memory: 18564 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.java
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        int result;
         for (int i = 0; i < n; i++) {
             int x1 = sc.nextInt();
             int y1 = sc.nextInt();
@@ -11,41 +11,34 @@ import java.util.Scanner;
             int x2 = sc.nextInt();
             int y2 = sc.nextInt();
             int r2 = sc.nextInt();
-            int bigRadius = Math.max(r1, r2);
-            int smallRadius = Math.min(r1, r2);
-            double distance = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+            int bigR = Math.max(r1, r2);
+            int smallR = Math.min(r1, r2);
+            double dis = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
             int radiusSum = (r1+r2);
-            if(distance==0) {
-                //두 원의 중심이 일치할 때
+            if(dis==0) {
                 if (r1 == r2) {
-                    arr[i]=-1;
+                    result=-1;
                 } else {
-                    arr[i]=0;
+                    result=0;
                 }
-            } else if(distance>bigRadius) {
-                //두 원이 서로 떨어져 있을 때
-                if(distance <radiusSum){
-                    arr[i]=2;
-                }else if (distance ==radiusSum) {
-                    arr[i]=1;
+            } else if(dis>bigR) {
+                if(dis <radiusSum){
+                    result=2;
+                }else if (dis ==radiusSum) {
+                    result=1;
                 } else {
-                    arr[i]=0;
+                    result=0;
                 }
-            } else if(distance<=bigRadius) {
-                //큰 원 안에 작은 원이 있을 때
-                if(distance==bigRadius||distance>bigRadius-smallRadius) {
-                    //두 점의 거리와 큰 원의 반지름이 같을 때
-                    arr[i]=2;
-                } else if(distance==bigRadius-smallRadius) {
-                    //두 점의 거리 == 큰 반지름-작은 반지름(내접)
-                    arr[i]=1;
+            } else {
+                if(dis==bigR||dis>bigR-smallR) {
+                    result=2;
+                } else if(dis==bigR-smallR) {
+                    result=1;
                 } else{
-                    arr[i]=0;
+                    result=0;
                 }
             }
-        }
-        for (int j : arr) {
-            System.out.println(j);
+            System.out.println(result);
         }
     }
 }
