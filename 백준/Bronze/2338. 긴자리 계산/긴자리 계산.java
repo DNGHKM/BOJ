f5d2cee2e6ce9e8fdcd0f1af1f6f1a32b7dd485f
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-10-14 23:12:14 +0900

    [Bronze V] Title: 긴자리 계산, Time: 140 ms, Memory: 14900 KB -BaekjoonHub

--- 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -1,19 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.math.BigInteger;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BigInteger a = new BigInteger(br.readLine());
         BigInteger b = new BigInteger(br.readLine());
-        sb.append(a.add(b)).append("\n");
-        sb.append(a.subtract(b)).append("\n");
-        sb.append(a.multiply(b)).append("\n");
-        System.out.println(sb);
+        bw.write(a.add(b)+"\n");
+        bw.write(a.subtract(b)+"\n");
+        bw.write(a.multiply(b)+"\n");
+        bw.close();
     }
 }
