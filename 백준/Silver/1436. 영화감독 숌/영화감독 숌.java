6292b7c05d36ecc582609f1f68fb2540b2dd8ae1
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-10-14 21:51:54 +0900

    [Silver V] Title: 영화감독 숌, Time: 368 ms, Memory: 87636 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -3,14 +3,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[2666800];
         int j =1;
-        for (int i = 0; i <arr.length; i++) {
-            if(Integer.toString(i).contains("666")) {
-                arr[j] = i;
-                j++;
+        int count = 0;
+        while(count!=n) {
+            j++;
+            if(Integer.toString(j).contains("666")) {
+               count++;
             }
         }
-        System.out.println(arr[n]);
+        System.out.println(j);
     }
 }
