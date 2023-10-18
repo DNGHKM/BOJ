dec9de23ca03771b4b0c16d45c29e0f48bef7fc7
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-10-18 20:20:28 +0900

    [Bronze II] Title: 수 정렬하기, Time: 168 ms, Memory: 16224 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,15 +1,20 @@
+import java.io.*;
 import java.util.*;
-public class Main{
-    public static void main(String[]args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            }
-        Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            System.out.println(arr[i]);
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ArrayList<Integer> al = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i <n; i++) {
+            al.add(Integer.parseInt(br.readLine()));
         }
+        Collections.sort(al);
+        for(int i : al) {
+            bw.write(i+"\n");
+        }
+        bw.flush();
+        bw.close();
     }
 }
