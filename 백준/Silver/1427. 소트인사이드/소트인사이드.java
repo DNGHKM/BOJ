f92fc7ccee7a0d471ce8735dc6fbd89dc585197a
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-10-18 20:51:35 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,21 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-public class Main {
+
+public class Main { //선택정렬 이용
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr= new int[(int)Math.log10(n)+1];
-        StringBuilder sf = new StringBuilder();
-        for (int i = 0; i <arr.length; i++) {
-            arr[i]=n%10;
-            n/=10;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ArrayList<Integer> al = new ArrayList<>();
+        String n = br.readLine();
+        for (int i = 0; i <n.length(); i++) {
+            al.add((int)n.charAt(i));
         }
-        Arrays.sort(arr);
-        for (int i = arr.length-1; i>=0; i--) {
-            sf.append(arr[i]);
+        for (int i = 0; i <al.size(); i++) {
+            int maxIndex =0;
+            int max = 0;
+            for (int j = i; j <al.size(); j++) {
+                if(al.get(j)>max){
+                    max=al.get(j);
+                    maxIndex = j;
+                }
+            }
+            al.set(maxIndex,al.get(i));
+            al.set(i,max);
         }
-        System.out.println(sf);
+        for(int i : al){
+            bw.write(i);
+        }
+        bw.flush();
+        bw.close();
     }
 }
