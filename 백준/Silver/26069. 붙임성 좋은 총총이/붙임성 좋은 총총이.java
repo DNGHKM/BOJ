d267f28afdc2d9f445367225bd2a54b53815cc38
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-10-22 22:41:03 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 152 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         HashSet<String> dance = new HashSet<>();
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i <n; i++) {
@@ -12,15 +13,12 @@ public class Main {
             StringTokenizer st = new StringTokenizer(input);
             String man1 = st.nextToken();
             String man2 = st.nextToken();
-            if(input.contains("ChongChong")){
-                dance.add(man1);
-                dance.add(man2);
-            }
-            if(dance.contains(man1)||dance.contains(man2)){
+            if(dance.contains(man1)||dance.contains(man2)||input.contains("ChongChong")){
                 dance.add(man1);
                 dance.add(man2);
             }
         }
-        System.out.println(dance.size());
+        bw.write(dance.size()+"");
+        bw.flush();
     }
 }
