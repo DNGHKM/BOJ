47e90b937db923fb2056c4f7a98b00ae3331f735
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-11-28 22:00:21 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 228 ms, Memory: 19160 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -6,29 +6,12 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         long min = Long.parseLong(st.nextToken());
         long max = Long.parseLong(st.nextToken());
-        int[] arr = new int[(int) Math.sqrt(max)+1];
-        ArrayList<Integer> primeList = new ArrayList<>();
-        for (int i = 2; i < arr.length; i++) {
-            arr[i] = i;
-        }
-        for (int i = 2; i <Math.sqrt((int) (max-min+1)); i++) {
-            for (int j = i+i; j <(int) Math.sqrt(max-min+1); j+=i) {
-                if(j%i==0){
-                    arr[j] = 0;
-                }
-            }
-        }
-        for(int i =0; i<arr.length; i++){
-            if(arr[i]!=0){
-                primeList.add(i);
-            }
-        }
         boolean[] result = new boolean[(int) (max-min+1)];
         Arrays.fill(result, true);
         long count = max-min+1;
 
-        for (long a : primeList) {
-            long s = a * a;
+        for (int a = 2; a<Math.sqrt(max)+1; a++) {
+            long s = (long) a * a;
             long start;
             if(min%s == 0){
                 start = min/s;
