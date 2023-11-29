f6ab11168415e350e28f074f338d77f03867f6ef
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-11-29 22:11:02 +0900

    [Bronze IV] Title: 평균 점수, Time: 120 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum =0;
+        for (int i = 0; i <5; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if(input<40) sum+=40;
+            else sum+=input;
+        }
+        System.out.println(sum/5);
+    }
+}
