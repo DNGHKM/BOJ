f51eb9abf41e67304fb1f9ebb577ac51bb13da1e
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2023-12-04 21:50:46 +0900

    [Silver II] Title: 수열의 합, Time: 148 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Silver/1024. 수열의 합/수열의 합.java
+++ 백준/Silver/1024. 수열의 합/수열의 합.java
@@ -10,19 +10,10 @@ public class Main {
         int l = Integer.parseInt(st.nextToken());
         int nDouble = n*2;
         boolean result = false;
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 2; i <=Math.sqrt(nDouble); i++) {
-            if(nDouble%i==0){
-                list.add(i);
-                list.add(nDouble/i);
-            }
-        }
-        list.add(nDouble);
-        Collections.sort(list);
-        for (int a : list) {
-           double first = (double) (nDouble-a*a+a)/(2*a);
-            if(l<=a&&a<=100&&!result&&first>=0&&first*10%10==0){
-                for (int j = 0; j <a; j++) {
+        for (int i=0; i <=100; i++) {
+           double first = (double) (nDouble-i*i+i)/(2*i);
+            if(l<=i&&!result&&first>=0&&first*10%10==0){
+                for (int j = 0; j <i; j++) {
                     bw.write((int)first+" ");
                     first++;
                     result = true;
