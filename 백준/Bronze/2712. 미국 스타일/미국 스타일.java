ce9d8928b94e1e58184b5b107debd935bba8e621
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-01-01 00:28:33 +0900

    [Bronze III] Title: 미국 스타일, Time: 176 ms, Memory: 16596 KB -BaekjoonHub

---
+++ 백준/Bronze/2712. 미국 스타일/미국 스타일.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i <n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double num = Double.parseDouble(st.nextToken());
+            String unit = st.nextToken();
+            double result;
+            if(unit.equals("kg")){
+               result = num*2.2046;
+               unit="lb";
+            }else if(unit.equals("lb")){
+                result=num*0.4536;
+                unit="kg";
+            }else if(unit.equals("l")){
+                result=num*0.2642;
+                unit="g";
+            }else{
+                result=num*3.7854;
+                unit="l";
+            }
+            result*=10000;
+            double resultRound =(double)Math.round(result)/10000;
+            String s = String.format("%.4f",resultRound);
+            bw.write(s+" "+unit+"\n");
+        }
+        bw.flush();
+    }
+}
