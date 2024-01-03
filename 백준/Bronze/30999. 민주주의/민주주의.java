5e4cd5cde4b89c6eaaf4487b821b5dc03f1fc55b
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-01-03 21:06:22 +0900

    [Bronze IV] Title: 민주주의, Time: 224 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Bronze/30999. 민주주의/민주주의.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int result = 0;
+        for (int i = 0; i <n; i++) {
+            String input = sc.next();
+            int count = 0;
+            for (int j = 0; j <m; j++) {
+                if(input.charAt(j)=='O'){
+                    count++;
+                }
+            }
+            if(count>m/2){
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
