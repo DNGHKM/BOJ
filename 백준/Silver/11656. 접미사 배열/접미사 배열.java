78d7eac4af582c329f70f869a9635394c11a4c45
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-01-04 21:35:47 +0900

    [Silver IV] Title: 접미사 배열, Time: 284 ms, Memory: 18492 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        ArrayList<String> arr = new ArrayList<>();
+        String[] arr = new String[s.length()];
         for (int i = 0; i <s.length(); i++) {
-            arr.add(s.substring(i));
+            arr[i]=s.substring(i);
         }
-        arr.sort(Comparator.naturalOrder());
+        Arrays.sort(arr);
         for (String a : arr) {
             System.out.println(a);
         }
