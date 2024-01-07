f4c439aba5ea9c392cc234fda8ceed1d3872ee24
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-01-07 11:51:14 +0900

    [Bronze III] Title: 저작권, Time: 204 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/2914. 저작권/저작권.java
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -2,8 +2,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        System.out.print(n*(m-1)+1);
+        System.out.print(sc.nextInt()*(sc.nextInt()-1)+1);
     }
 }
