6d33af83b850d44ecc66dfb2436f19ecf88fbeb2
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-02-19 10:28:45 +0900

    [Silver V] Title: 스네이크버드, Time: 132 ms, Memory: 14548 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i : arr) {
+            if(i<=l){
+                l++;
+            }else{
+                System.out.print(l);
+                return;
+            }
+        }
+        System.out.print(l);
+    }
+}
