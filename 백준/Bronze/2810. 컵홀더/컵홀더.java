21e076ba40db70dc763f4020614253482e4d81f5
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-02-21 12:41:07 +0900

    [Bronze I] Title: 컵홀더, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/2810. 컵홀더/컵홀더.java
+++ 백준/Bronze/2810. 컵홀더/컵홀더.java
@@ -1,18 +1,11 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String s = sc.next();
-        int c = 1;
-        int l = 0;
-        for (int i = 0; i <s.length(); i++) {
-            if(s.charAt(i)=='S') c++;
-            else l++;
-        }
-        c+=l/2;
-        if(n<c) c=n;
+        int c = 1+s.length()-s.replace("S","").length()+(s.length()-s.replace("L","").length())/2;
+        c = (n<c) ? n : c;
         System.out.print(c);
     }
 }
