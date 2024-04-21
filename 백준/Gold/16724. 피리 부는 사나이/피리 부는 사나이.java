4622d0547f0437f629346abedee5c4db8cc86064
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-04-21 16:26:44 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 364 ms, Memory: 47912 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -2,52 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static char[][] arr;
-    static int[][] group;
-    static int ans;
-    static int num;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        arr = new char[n][m];
-        group = new int[n][m];
-        ans = 0;
-        num = 1;
+        char[][] arr = new char[n][m];
+        int[][] group = new int[n][m];
+        int ans = 0;
+        int num = 1;
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            for (int j = 0; j < s.length(); j++) {
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = s.charAt(j);
             }
         }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (group[i][j] == 0) {
-                    dfs(i, j);
+                    int[] cur = {i, j};
+                    while (group[cur[0]][cur[1]] == 0) {
+                        group[cur[0]][cur[1]] = num;
+                        char c = arr[cur[0]][cur[1]];
+                        if (c == 'D') {
+                            cur[0]++;
+                        } else if (c == 'U') {
+                            cur[0]--;
+                        } else if (c == 'R') {
+                            cur[1]++;
+                        } else {
+                            cur[1]--;
+                        }
+                    }
+                    if (group[cur[0]][cur[1]] == num) ans++;
                     num++;
                 }
             }
         }
         System.out.print(ans);
-    }
-
-    static void dfs(int i, int j) {
-        int[] cur = {i, j};
-        while (group[cur[0]][cur[1]] == 0) {
-            group[cur[0]][cur[1]] = num;
-            char c = arr[cur[0]][cur[1]];
-            if (c == 'D') {
-                cur[0] += 1;
-            } else if (c == 'U') {
-                cur[0] -= 1;
-            } else if (c == 'R') {
-                cur[1] += 1;
-            } else {
-                cur[1] -= 1;
-            }
-        }
-        if (group[cur[0]][cur[1]] == num) ans++;
     }
 }
