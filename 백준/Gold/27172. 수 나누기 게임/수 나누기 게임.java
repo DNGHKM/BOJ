093461804b5fddbf730e1e9b0168348ac6bbab0a
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-04-24 15:59:39 +0900

    [Gold V] Title: 수 나누기 게임, Time: 1312 ms, Memory: 57592 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -5,29 +5,20 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int num = 1_000_001;
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        int num = Arrays.stream(arr).max().getAsInt() + 1;
         int[] point = new int[num];
         boolean[] exist = new boolean[num];
         Arrays.stream(arr).forEach(i -> exist[i] = true);
         for (int k : arr) {
-            HashSet<Integer> set = new HashSet<>();
-            for (int i = 1; i < Math.sqrt(k) + 1; i++) {
-                if (k % i == 0) {
-                    set.add(i);
-                    if(k!=1) {
-                        set.add(k / i);
-                    }
-                }
-            }
-            for (Integer i : set) {
+            for (int i = k + k; i < num; i += k) {
                 if (exist[i]) {
-                    point[i]++;
-                    point[k]--;
+                    point[i]--;
+                    point[k]++;
                 }
             }
         }
