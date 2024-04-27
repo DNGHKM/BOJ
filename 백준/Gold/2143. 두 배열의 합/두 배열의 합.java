892b9acc51bda6945f306064bc43a9683c932db9
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-04-27 17:56:13 +0900

    [Gold III] Title: 두 배열의 합, Time: 868 ms, Memory: 68668 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -2,54 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br;
 
     public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         long count = 0;
 
         int a = Integer.parseInt(br.readLine());
-        long[] arrA = makeArr(a);
+        long[] arrA = new long[a];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < a; i++) {
+            arrA[i] = Integer.parseInt(st.nextToken());
+        }
 
         int b = Integer.parseInt(br.readLine());
-        long[] arrB = makeArr(b);
-
-        Map<Long, Integer> mapA = fillMap(arrA);
-        Map<Long, Integer> mapB = fillMap(arrB);
-
-
-        for (Long i : mapA.keySet()) {
-            long target = t - i;
-            if (mapB.containsKey(target)) {
-                count += (long) mapA.get(i) * mapB.get(target);
-            }
+        long[] arrB = new long[b];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < b; i++) {
+            arrB[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.print(count);
-    }
 
-    static long[] makeArr(int size) throws IOException {
-        long[] arr = new long[size];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
-        }
-        return arr;
-    }
-
-    static Map<Long, Integer> fillMap(long[] arr) {
-        TreeMap<Long, Integer> map = new TreeMap<>();
-        for (int i = 0; i < arr.length; i++) {
+        Map<Long, Integer> mapA = new TreeMap<>();
+        for (int i = 0; i < arrA.length; i++) {
             long sum = 0;
-            for (int j = i; j < arr.length; j++) {
-                long num = sum += arr[j];
-                if (map.containsKey(num)) {
-                    map.put(num, map.get(num) + 1);
+            for (int j = i; j < arrA.length; j++) {
+                sum += arrA[j];
+                if (mapA.containsKey(sum)) {
+                    mapA.put(sum, mapA.get(sum) + 1);
                 } else {
-                    map.put(num, 1);
+                    mapA.put(sum, 1);
                 }
             }
         }
-        return map;
+
+        for (int i = 0; i < arrB.length; i++) {
+            long sum = 0;
+            for (int j = i; j < arrB.length; j++) {
+                sum += arrB[j];
+                if (mapA.containsKey(t - sum)) {
+                    count += (long) mapA.get(t - sum);
+                }
+            }
+        }
+
+        System.out.print(count);
     }
 }
