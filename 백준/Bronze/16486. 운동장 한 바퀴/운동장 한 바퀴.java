4cbb3f6eea5c5a91ac40df6b57f2efb761f79452
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-04-28 01:29:21 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 212 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double d1 = sc.nextDouble();
+        double d2 = sc.nextDouble();
+        System.out.print(2 * d1 + 2*d2 * 3.141592);
+    }
+}
