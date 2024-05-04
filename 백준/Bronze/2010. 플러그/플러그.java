faa317b34d2cc56ac029da2bc6320f91d8e94e9a
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-05-04 12:52:08 +0900

    [Bronze III] Title: 플러그, Time: 280 ms, Memory: 41404 KB -BaekjoonHub

--- 백준/Bronze/2010. 플러그/플러그.java
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -1,12 +1,12 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            ans += sc.nextInt();
+            ans += Integer.parseInt(br.readLine());
             if (i >= 1) ans--;
         }
         System.out.println(ans);
