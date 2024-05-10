5c2b8da570528679f0407e1dbf132e25333cfd94
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-05-11 08:02:40 +0900

    [Bronze V] Title: CASIO, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+//15963
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if(sc.nextLong()==sc.nextLong()){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
