d8fa216a6a0ee1c93d0c4fc6eb4906e3c81021b4
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-05-17 16:50:32 +0900

    [Gold IV] Title: 타임머신, Time: 964 ms, Memory: 66352 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final long INF = Long.MAX_VALUE;
+    static ArrayList<int[]> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        long[] dist = new long[n + 1];
+        for (int i = 2; i < dist.length; i++) {
+            dist[i] = INF;
+        }
+        list = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            list.add(new int[]{s, e, d});
+        }
+        for (int i = 0; i < n - 1; i++) {
+            bellman(dist);
+        }
+        long[] tmpDist = Arrays.copyOf(dist, dist.length);
+        bellman(tmpDist);
+        for (int i = 0; i < dist.length; i++) {
+            if (dist[i] != tmpDist[i]) {
+                System.out.println(-1);
+                return;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i < dist.length; i++) {
+            if (dist[i] != INF) {
+                sb.append(dist[i] + "\n");
+            } else {
+                sb.append(-1 + "\n");
+            }
+        }
+        System.out.print(sb);
+    }
+
+    private static void bellman(long[] arr) {
+        ArrayList<Integer> notInf = new ArrayList<>();
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[j] != INF) notInf.add(j);
+        }
+        for (int[] ints : list) {
+            if (notInf.contains(ints[0])) {
+                if (arr[ints[0]] + ints[2] < arr[ints[1]]) {
+                    arr[ints[1]] = arr[ints[0]] + ints[2];
+                }
+            }
+        }
+    }
+}
