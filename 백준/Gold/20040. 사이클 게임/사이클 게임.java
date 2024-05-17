052c789ab9d1a4a1322b25fa3e0348f7e0c9fa8b
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-05-17 15:13:57 +0900

    [Gold IV] Title: 사이클 게임, Time: 708 ms, Memory: 126084 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     static int[] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,20 +13,17 @@ public class Main {
             arr[i] = i;
         }
         int count = 0;
-        ArrayList<int[]> list = new ArrayList<>();
         for (int i = 0; i < m; i++) {
+            count++;
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            list.add(new int[]{a, b});
-        }
-        for (int[] ints : list) {
-            count++;
-            if (find(ints[0]) == find(ints[1])) {
+            if(find(a)!=find(b)){
+                union(a, b);
+            }else{
                 System.out.print(count);
                 return;
             }
-            union(ints[0], ints[1]);
         }
         System.out.print(0);
     }
@@ -35,15 +31,15 @@ public class Main {
     static void union(int a, int b) {
         int parentA = find(a);
         int parentB = find(b);
-        if (parentA < parentB) {
+        if (parentA<parentB) {
             arr[parentB] = parentA;
-        } else {
+        }else{
             arr[parentA] = parentB;
         }
     }
 
     static int find(int i) {
-        if (arr[i] == i) {
+        if(arr[i]==i){
             return i;
         }
         return arr[i] = find(arr[i]);
