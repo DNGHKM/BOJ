b0b37f4dc0c0c3ca6c322b7d6d1ee4a45e8ea6b0
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-05-19 20:48:47 +0900

    [Platinum V] Title: 히스토그램, Time: 288 ms, Memory: 27276 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.java
+++ 백준/Platinum/1725. 히스토그램/히스토그램.java
@@ -6,36 +6,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long area = 0;
         int n = Integer.parseInt(br.readLine());
-        long[] arr = new long[n];
-        long[] leftIdx = new long[n];
-        long[] rightIdx = new long[n];
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
+
         Stack<long[]> stack = new Stack<>();
-        stack.add(new long[]{arr[0], 0}); //값, 인덱스 순
-        for (int i = 1; i < arr.length; i++) {
-            leftIdx[i] = i;
-            long num = i;
-            while (!stack.isEmpty() && arr[i] <= stack.peek()[0]) {
-                num = stack.pop()[1];
+        for (int i = 0; i < n; i++) {
+            int height = arr[i];
+            int start = i;
+            while (!stack.isEmpty() && stack.peek()[1] > height) {
+                long[] pop = stack.pop();
+                long h = pop[1];
+                long w = i - pop[0];
+                area = Math.max(area, h * w);
+                start = (int) pop[0];
             }
-            leftIdx[i] = num;
-            stack.push(new long[]{arr[i], num});
+            stack.push(new long[]{start, height});
         }
-        stack.clear();
-        stack.add(new long[]{arr[arr.length - 1], arr.length - 1}); //값, 인덱스 순
-        for (int i = arr.length - 1; i >= 0; i--) {
-            rightIdx[i] = i;
-            long num = i;
-            while (!stack.isEmpty() && arr[i] <= stack.peek()[0]) {
-                num = stack.pop()[1];
-            }
-            rightIdx[i] = num;
-            stack.push(new long[]{arr[i], num});
-        }
-        for (int i = 0; i < arr.length; i++) {
-            area = Math.max(area, arr[i] * (rightIdx[i] - leftIdx[i] + 1));
+        while (!stack.isEmpty()) {
+            long[] pop = stack.pop();
+            long h = pop[1];
+            long w = n - pop[0];
+            area = Math.max(area, h * w);
         }
         System.out.println(area);
     }
