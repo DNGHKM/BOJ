f4e8c69d15345010cde4d7e296b8e8435c1cf1a7
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-06-10 16:35:48 +0900

    [Silver III] Title: All Your Base (Small), Time: 156 ms, Memory: 16168 KB -BaekjoonHub

---
+++ 백준/Silver/12637. All Your Base （Small）/All Your Base （Small）.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            String input = br.readLine();
+            int num = 2;
+            for (int j = 0; j < input.length(); j++) {
+                if (j == 0) {
+                    map.put(input.charAt(j), 1);
+                }
+                if(map.size()==1&&!map.containsKey(input.charAt(j))){
+                    map.put(input.charAt(j), 0);
+                }
+                if(!map.containsKey(input.charAt(j))){
+                    map.put(input.charAt(j), num++);
+                }
+            }
+            int radix = Math.max(map.size(),2);
+            long time = 0;
+            for (int j = input.length() - 1; j >= 0; j--) {
+                time += (long) ((map.get(input.charAt(j)) * Math.pow(radix, input.length() - 1 - j)));
+            }
+            sb.append("Case #"+(i+1)+": "+time+"\n");
+        }
+        System.out.println(sb);
+    }
+}
