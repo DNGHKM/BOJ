161a644cb813ccbde51c96be2cee7a7235b1eb81
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-07-13 15:25:45 +0900

    [Silver II] Title: Great Pow!, Time: 188 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Silver/10895. Great Pow！/Great Pow！.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int pow = sc.nextInt();
+        int ans = 0;
+        if (num % 2 == 0) {
+            if (pow == 0) {
+                ans = num;
+            } else {
+                ans = 1;
+            }
+        } else {
+            ans = num;
+        }
+        System.out.println(ans);
+    }
+}
