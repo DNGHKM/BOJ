97b42681b41bd017207ebcf27709de46b39de5c9
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-07-21 21:16:45 +0900

    [Silver I] Title: 언더프라임, Time: 2104 ms, Memory: 20276 KB -BaekjoonHub

---
+++ 백준/Silver/1124. 언더프라임/언더프라임.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean[] prime = new boolean[m + 1];
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            for (int j = i + i; j < prime.length; j += i) {
+                prime[j] = false;
+            }
+        }
+        int ans = 0;
+        for (int i = n; i <= m; i++) {
+            int count = 0;
+            int num = i;
+            while (num > 1) {
+                for (int j = 2; j < prime.length; j++) {
+                    if (prime[j] && num % j == 0) {
+                        num /= j;
+                        count++;
+                        break;
+                    }
+                }
+            }
+            if (prime[count]) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
