c4936b6cc2b5f5e33d4a1e86fd20dafa9d2f0878
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-07-26 14:47:12 +0900

    [Bronze I] Title: 단어 공부, Time: 476 ms, Memory: 28988 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,47 +1,27 @@
 import java.util.*;
-public class Main {
+
+class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        char[] c = new char[input.length()];
-        int[][] count = new int[26][2];
-        int rightMax = 0;
-        int rightAddress = 0;
-        int leftMax = 0;
-        int leftAddress = 0;
-        for (int i = 0; i < input.length(); i++) {
-            c[i] = input.charAt(i);
-            if('a'<=c[i]&&c[i]<='z'){
-                c[i]-=32;
+        String s = sc.next().toUpperCase();
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'A']++;
+        }
+        int max = 0;
+        int maxIdx = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                maxIdx = i;
             }
         }
-        for (int i = 0; i < 26; i++) {
-            count[i][0] = i+65;
-        }
-        for (int i = 0; i < c.length; i++) {
-            for (int j = 0; j < 26; j++) {
-                if(c[i]==j+65){
-                    count[j][1]+=1;
-                }
-            }
-        }
-        for (int i = 0; i < 26; i++) {
-                if(rightMax < count[i][1]){
-                    rightMax = count[i][1];
-                    rightAddress = i;
-                }
-            }
-        for (int i = 25; i >=0; i--) {
-                if(leftMax < count[i][1]){
-                    leftMax = count[i][1];
-                    leftAddress = i;
-                }
-            }
-        if (rightAddress==leftAddress){
-            char result = (char) (rightAddress+65);
-            System.out.println(result);
-        } else{
+        int k = max;
+        long count = Arrays.stream(arr).filter(i -> i == k).count();
+        if (count != 1) {
             System.out.println("?");
+            return;
         }
+        System.out.println((char) (maxIdx + 'A'));
     }
 }
