9b8db2fc531a2f02927127de3d3c40f7bc39fbd4
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-07-29 15:22:27 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -0,0 +1,9 @@
+select 
+    count(FISH_TYPE) as FISH_COUNT,
+    max(LENGTH) as MAX_LENGTH,
+    FISH_TYPE
+from FISH_INFO 
+group by FISH_TYPE
+having
+    avg(if(LENGTH is null, 10, LENGTH)) >= 33
+order by FISH_TYPE asc
