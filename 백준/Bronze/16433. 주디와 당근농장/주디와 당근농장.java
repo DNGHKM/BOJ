85374497a3b74a44a1046badfa514ea0f462e197
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-08-01 08:59:49 +0900

    [Bronze I] Title: 주디와 당근농장, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/16433. 주디와 당근농장/주디와 당근농장.java
+++ 백준/Bronze/16433. 주디와 당근농장/주디와 당근농장.java
@@ -6,27 +6,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
         int n = sc.nextInt();
-        char[][] arr = new char[n][n];
-        Arrays.stream(arr).forEach(a -> Arrays.fill(a,'.'));
         int r = sc.nextInt();
         int c = sc.nextInt();
-        fill((r + c) % 2, arr);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                sb.append(arr[i][j]);
+                if ((r + c) % 2 == (i + j) % 2) {
+                    sb.append('v');
+                }else{
+                    sb.append('.');
+                }
             }
             sb.append("\n");
         }
         System.out.print(sb);
-    }
-
-    static void fill(int num, char[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if ((i + j) % 2 == num) {
-                    arr[i][j] = 'v';
-                }
-            }
-        }
     }
 }
