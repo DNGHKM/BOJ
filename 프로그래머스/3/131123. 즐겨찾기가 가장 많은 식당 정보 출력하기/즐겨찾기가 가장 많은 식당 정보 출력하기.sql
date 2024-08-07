14c94fa143b41c573bb55ce3ead4573f60cebd94
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-08-07 17:04:01 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+SELECT
+a.FOOD_TYPE,
+a.REST_ID,
+a.REST_NAME,
+a.FAVORITES
+from REST_INFO a, 
+(select 
+    FOOD_TYPE,
+    max(FAVORITES) as FAVORITES
+    from REST_INFO
+    group by FOOD_TYPE
+) b
+where a.FOOD_TYPE = b.FOOD_TYPE and a.FAVORITES = b.FAVORITES
+order by food_type desc
