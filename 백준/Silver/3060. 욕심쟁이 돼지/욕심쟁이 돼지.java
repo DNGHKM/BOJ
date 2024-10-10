5e3cfabb05419e4e1acf830820d2c9a161d452a6
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-10-10 09:08:18 +0900

    [Silver V] Title: 욕심쟁이 돼지, Time: 124 ms, Memory: 15980 KB -BaekjoonHub

---
+++ 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[6];
+            int ans = 1;
+            int sum = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < arr.length; j++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            while (n >= sum) {
+                sum *= 4;
+                ans++;
+            }
+            bw.write(ans+"\n");
+        }
+        bw.flush();
+    }
+}
