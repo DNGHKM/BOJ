83e2bf246196ad4c5c2aa11b4c286bc946944b6a
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-10-20 22:06:59 +0900

    [Bronze V] Title: 母音を数える (Counting Vowels), Time: 164 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/18409. 母音を数える （Counting Vowels）/母音を数える （Counting Vowels）.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
