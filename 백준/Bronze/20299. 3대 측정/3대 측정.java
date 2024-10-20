cfbab61a2040583ddc63146f70123773cb090be7
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-10-21 08:48:53 +0900

    [Bronze II] Title: 3대 측정, Time: 952 ms, Memory: 200728 KB -BaekjoonHub

--- 백준/Bronze/20299. 3대 측정/3대 측정.java
+++ 백준/Bronze/20299. 3대 측정/3대 측정.java
@@ -4,14 +4,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int l = Integer.parseInt(st.nextToken());
         int[] arr = new int[3];
         int cnt = 0;
-        ArrayList<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int sum = 0;
@@ -22,13 +21,9 @@ public class Main {
             }
             if (sum >= k && min >= l) {
                 cnt++;
-                Arrays.stream(arr).forEach(list::add);
+                Arrays.stream(arr).forEach(num -> sb.append(num+" "));
             }
         }
-        bw.write(cnt + "\n");
-        for (Integer i : list) {
-            bw.write(i + " ");
-        }
-        bw.flush();
+        System.out.print(cnt+"\n"+sb);
     }
 }
