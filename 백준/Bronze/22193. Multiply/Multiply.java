d81b46b04acfd7609383a2115701fd2b38df72fc
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-11-01 15:53:08 +0900

    [Bronze V] Title: Multiply, Time: 620 ms, Memory: 55760 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -1,11 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
-        System.out.println(Long.parseLong(br.readLine()) * Long.parseLong(br.readLine()));
+        System.out.println(new BigInteger(br.readLine()).multiply(new BigInteger(br.readLine())));
     }
 }
