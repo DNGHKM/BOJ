dc8b869b39048793c2990fe624dba809f689d109
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2024-11-21 08:44:30 +0900

    [Bronze V] Title: Serca, Time: 340 ms, Memory: 56928 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            sb.append(" @@@   @@@ \n" +
+                    "@   @ @   @\n" +
+                    "@    @    @\n" +
+                    "@         @\n" +
+                    " @       @ \n" +
+                    "  @     @  \n" +
+                    "   @   @   \n" +
+                    "    @ @    \n" +
+                    "     @     \n");
+        }
+        System.out.println(sb);
+    }
+}
