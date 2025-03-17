8b2a3371a517b773c824d80e4d372ceb17b957bf
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2025-03-17 15:43:53 +0900

    [Silver III] Title: 착신 전환 소동, Time: 416 ms, Memory: 30468 KB -BaekjoonHub

--- 백준/Silver/31409. 착신 전환 소동/착신 전환 소동.java
+++ 백준/Silver/31409. 착신 전환 소동/착신 전환 소동.java
@@ -1,9 +1,10 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
@@ -15,7 +16,10 @@ public class Main {
                 arr[i] = i == n - 1 ? 1 : arr[i] + 1;
             }
         }
-        System.out.println(cnt);
-        Arrays.stream(arr).forEach(i -> System.out.print(i + " "));
+        bw.write(cnt + "\n");
+        for (int i : arr) {
+            bw.write(i + " ");
+        }
+        bw.flush();
     }
 }
