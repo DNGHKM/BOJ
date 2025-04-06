9c32c12bc5d6d3e2ad53a32e4dc85ee3bbeb651a
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2025-04-06 16:33:58 +0900

    [Bronze IV] Title: 등교, Time: 104 ms, Memory: 14224 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31962. 등교/등교.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int max = -1;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            if ((s + t) > x) {
+                continue;
+            }
+            max = Math.max(max, s);
+        }
+        System.out.println(max);
+    }
+}
