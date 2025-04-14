cb068ff78553127179be8e23a31a2c7478f77ae4
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2025-04-14 14:50:13 +0900

    [Silver II] Title: 황소 다마고치, Time: 152 ms, Memory: 16476 KB -BaekjoonHub

---
+++ 백준/Silver/23323. 황소 다마고치/황소 다마고치.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long n = Long.parseLong(st.nextToken());
+            long m = Long.parseLong(st.nextToken());
+            sb.append((int) (Math.log10(n) / Math.log10(2)) + m + 1 + "\n");
+        }
+        System.out.println(sb);
+    }
+}
