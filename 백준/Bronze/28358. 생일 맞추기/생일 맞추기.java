95acadb9070e83dd9334e47db0bb65071dfb847c
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2025-07-13 13:20:35 +0900

    [Bronze I] Title: 생일 맞추기, Time: 304 ms, Memory: 31564 KB -BaekjoonHub

--- 백준/Bronze/28358. 생일 맞추기/생일 맞추기.java
+++ 백준/Bronze/28358. 생일 맞추기/생일 맞추기.java
@@ -1,50 +1,50 @@
-import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
+        int[] days = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int n = sc.nextInt();
-        int[] arr = new int[10];
         for (int i = 0; i < n; i++) {
+            int[] arr = new int[10];
             for (int j = 0; j < 10; j++) {
                 arr[j] = sc.nextInt();
             }
 
-            LocalDate start = LocalDate.of(2024, 1, 1);
-            LocalDate end = LocalDate.of(2024, 12, 31);
-            LocalDate curr = start;
-
-            int cnt = 0;
-            while (!curr.isAfter(end)) {
-                if (check(curr, arr)) {
-                    cnt++;
-                }
-                curr = curr.plusDays(1);
-            }
+            int cnt = getCnt(arr, days);
             sb.append(cnt + "\n");
         }
         System.out.println(sb);
     }
 
-    private static boolean check(LocalDate curr, int[] arr) {
-        String[] split = curr.toString().split("-");
-        String month = String.valueOf(Integer.parseInt(split[1]));
-        String day = String.valueOf(Integer.parseInt(split[2]));
-
-        for (int i = 0; i < month.length(); i++) {
-            if (arr[month.charAt(i) - '0'] == 1) {
-                return false;
+    private static int getCnt(int[] arr, int[] days) {
+        int cnt = 0;
+        for (int j = 1; j <= 12; j++) {
+            if ((j < 10 && arr[j] == 1)) {
+                continue;
             }
-        }
 
-        for (int i = 0; i < day.length(); i++) {
-            if (arr[day.charAt(i) - '0'] == 1) {
-                return false;
+            if (j >= 10) {
+                if (arr[j / 10] == 1 || arr[j % 10] == 1) {
+                    continue;
+                }
             }
+
+            for (int k = 1; k <= days[j]; k++) {
+                if ((k < 10 && arr[k] == 1)) {
+                    continue;
+                }
+
+                if (k >= 10) {
+                    if (arr[k / 10] == 1 || arr[k % 10] == 1) {
+                        continue;
+                    }
+                }
+                cnt++;
+            }
+
         }
-        return true;
+        return cnt;
     }
 }
-
