4abefb98a84f16e56db391d534802e4a2b866db3
DONGHA KIM <134900901+DNGHKM@users.noreply.github.com>
2025-07-22 13:21:21 +0900

    [Silver III] Title: 장난감 자물쇠, Time: 328 ms, Memory: 33096 KB -BaekjoonHub

--- 백준/Silver/32373. 장난감 자물쇠/장난감 자물쇠.java
+++ 백준/Silver/32373. 장난감 자물쇠/장난감 자물쇠.java
@@ -9,44 +9,18 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        if (k == 1) {
-            System.out.println("Yes");
-            return;
-        }
-
         int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
-        List<List<Integer>> groups = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            groups.add(new ArrayList<>());
-        }
-        for (int i = 0; i < n; i++) {
-            groups.get(i % k).add(arr[i]);
-        }
-
-
-        for (List<Integer> group : groups) {
-            Collections.sort(group);
-        }
-        
-        int[] sorted = new int[n];
-        int[] groupIdx = new int[k];
-        for (int i = 0; i < n; i++) {
-            int mod = i % k;
-            sorted[i] = groups.get(mod).get(groupIdx[mod]++);
-        }
 
         for (int i = 0; i < n; i++) {
-            if (sorted[i] != i) {
+            if ((i - arr[i]) % k != 0) {
                 System.out.println("No");
                 return;
             }
         }
-
         System.out.println("Yes");
     }
 }
